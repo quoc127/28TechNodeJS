715d3cd0b705be7157318aac20a5fc58bd4abb97
quoc127 <nquoc160200@gmail.com>
2024-05-06 08:51:15 +0700

    remove file

--- common/core_array.js
+++
@@ -1,8 +0,0 @@
-const checkOneElementExist = (userArray, callback) => {
-    for (let i = 0; i < userArray.length; i++) {
-        if (callback(userArray[i])) {
-            return true;
-        }
-    }
-    return false;
-};
