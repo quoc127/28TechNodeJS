30a564437003a079a1e777bdb01d0074104b9180
quoc127 <nquoc160200@gmail.com>
2024-05-06 21:58:29 +0700

    19. learn PUG template engine

--- vi-du-01/index.js
+++ vi-du-01/index.js
@@ -8,12 +8,20 @@ app.set('view engine', 'pug')
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-    res.render('index', {title: "Home page", message: "Welcome"})
+  res.render('index', { title: "Trang chủ", message: "Xin chào các bạn" })
 })
 
 app.get('/products', (req, res) => {
-    res.send("<h1>Products page</h1>")
-  })
+  res.send("<h1>Products page</h1>")
+})
+
+app.get('/blog', (req, res) => {
+  res.send("<h1>Trang danh sách bài viết</h1>")
+})
+
+app.get('/contact', (req, res) => {
+  res.render('contact', { title: "Trang liên hệ", message: "Xin chào các bạn" })
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
