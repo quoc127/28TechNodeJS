e11fac79ea47ca8b294a292a2c3760eb8789779c
quoc127 <nquoc160200@gmail.com>
2024-05-08 12:21:33 +0700

    21. add UI product page and connect mongoDB

--- product-management/index.js
+++ product-management/index.js
@@ -1,14 +1,16 @@
 const express = require("express");
-require('dotenv').config();
+require("dotenv").config();
 
 const route = require("./routes/client/index.route");
+
+const database = require("./config/database.js");
+database.connect();
 
 const app = express();
 const port = process.env.PORT;
 
 app.set("views", "./views");
 app.set("view engine", "pug");
-
 
 // use files in public folder
 app.use(express.static("public"));
