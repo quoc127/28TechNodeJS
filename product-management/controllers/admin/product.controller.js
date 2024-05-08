fb22d8ea30eee1583ade1b46bbaff0cff327a99f
quoc127 <nquoc160200@gmail.com>
2024-05-08 23:47:48 +0700

    22.admin - create UI product page - filter and search(FE)

---
+++ product-management/controllers/admin/product.controller.js
@@ -0,0 +1,50 @@
+const Product = require("../../models/product.model");
+
+// [GET] /admin/products
+module.exports.index = async (req, res) => {
+  const filterStatus = [
+    {
+      name: "Tất cả",
+      status: "",
+      class: ""
+    },
+    {
+      name: "Hoạt động",
+      status: "active",
+      class: ""
+    },
+    {
+      name: "Dừng hoạt động",
+      status: "inactive",
+      class: ""
+    }
+  ]
+
+  // Add class "active" for button filter product
+  if (req.query.status) {
+    const index = filterStatus.findIndex(item => item
+      .status == req.query.status
+    );
+    filterStatus[index].class = "active";
+  } else {
+    const index = filterStatus.findIndex(item => item
+      .status == ""
+    );
+    filterStatus[index].class = "active";
+  }
+
+  const find = {
+    deleted: false,
+  };
+  
+  if (req.query.status) {
+    find.status = req.query.status;
+  }
+  const products = await Product.find(find);
+
+  res.render("admin/pages/products/index.pug", {
+    pageTitle: "danh sách sản phẩm",
+    products: products,
+    filterStatus: filterStatus,
+  });
+};
--- product-management/index.js
+++ product-management/index.js
@@ -1,22 +1,28 @@
 const express = require("express");
 require("dotenv").config();
-
-const route = require("./routes/client/index.route");
-
-const database = require("./config/database.js");
-database.connect();
-
 const app = express();
 const port = process.env.PORT;
 
+// Routes
+const routeAdmin = require("./routes/admin/index.route");
+const route = require("./routes/client/index.route");
+route(app);
+routeAdmin(app);
+
+// Database
+const database = require("./config/database.js");
+database.connect();
+
+// Pug engine
 app.set("views", "./views");
 app.set("view engine", "pug");
 
 // use files in public folder
 app.use(express.static("public"));
 
-// Routes
-route(app);
+// App Locals Variables
+const systemConfig = require("./config/system.js");
+app.locals.prefixAdmin = systemConfig.prefixAdmin;
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
