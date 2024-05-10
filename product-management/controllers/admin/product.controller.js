6ca304812ea15b38a62e32a23dbda6f2ffcb188f
quoc127 <nquoc160200@gmail.com>
2024-05-10 09:17:53 +0700

    23.completed search function

--- product-management/controllers/admin/product.controller.js
+++ product-management/controllers/admin/product.controller.js
@@ -40,11 +40,21 @@ module.exports.index = async (req, res) => {
   if (req.query.status) {
     find.status = req.query.status;
   }
+
+  // Search function
+  let keyword = "";
+  if (req.query.keyword) {
+    keyword = (req.query.keyword);
+    const regex = new RegExp(keyword, "i");
+    find.title = regex;
+  }
+
   const products = await Product.find(find);
 
   res.render("admin/pages/products/index.pug", {
     pageTitle: "danh sách sản phẩm",
     products: products,
     filterStatus: filterStatus,
+    keyword: keyword,
   });
 };
