bcfb69ea9451b1e5df4e6adbb43836f8a282c82f
quoc127 <nquoc160200@gmail.com>
2024-05-10 09:44:34 +0700

    optimal code function filter

--- product-management/controllers/admin/product.controller.js
+++ product-management/controllers/admin/product.controller.js
@@ -1,37 +1,11 @@
 const Product = require("../../models/product.model");
 
+const filterStatusHelper = require("../../helpers/filterStatus")
+
 // [GET] /admin/products
 module.exports.index = async (req, res) => {
-  const filterStatus = [
-    {
-      name: "Tất cả",
-      status: "",
-      class: ""
-    },
-    {
-      name: "Hoạt động",
-      status: "active",
-      class: ""
-    },
-    {
-      name: "Dừng hoạt động",
-      status: "inactive",
-      class: ""
-    }
-  ]
-
-  // Add class "active" for button filter product
-  if (req.query.status) {
-    const index = filterStatus.findIndex(item => item
-      .status == req.query.status
-    );
-    filterStatus[index].class = "active";
-  } else {
-    const index = filterStatus.findIndex(item => item
-      .status == ""
-    );
-    filterStatus[index].class = "active";
-  }
+  // Filter function
+  const filterStatus = filterStatusHelper(req.query);
 
   const find = {
     deleted: false,
@@ -40,6 +14,7 @@ module.exports.index = async (req, res) => {
   if (req.query.status) {
     find.status = req.query.status;
   }
+  console.log(req.query.status);
 
   // Search function
   let keyword = "";
---
+++ product-management/helpers/filterStatus.js
@@ -0,0 +1,30 @@
+module.exports = (query) => {
+  const filterStatus = [
+    {
+      name: "Tất cả",
+      status: "",
+      class: "",
+    },
+    {
+      name: "Hoạt động",
+      status: "active",
+      class: "",
+    },
+    {
+      name: "Dừng hoạt động",
+      status: "inactive",
+      class: "",
+    },
+  ];
+
+  // Add class "active" for button filter product
+  if (query.status) {
+    const index = filterStatus.findIndex((item) => item.status == query.status);
+    filterStatus[index].class = "active";
+  } else {
+    const index = filterStatus.findIndex((item) => item.status == "");
+    filterStatus[index].class = "active";
+  }
+
+  return filterStatus;
+};
