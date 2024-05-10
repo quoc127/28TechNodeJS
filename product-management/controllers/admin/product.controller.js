b8140c775cbbe1aeaa4113b5b0f506d97e8cad54
quoc127 <nquoc160200@gmail.com>
2024-05-10 12:20:50 +0700

    23.admin - pagination product page

--- product-management/controllers/admin/product.controller.js
+++ product-management/controllers/admin/product.controller.js
@@ -1,8 +1,8 @@
 const Product = require("../../models/product.model");
 
 const filterStatusHelper = require("../../helpers/filterStatus");
-const searchrStatusHelper = require("../../helpers/search")
-
+const searchrStatusHelper = require("../../helpers/search");
+const paginationHelper = require("../../helpers/pagination");
 
 // [GET] /admin/products
 module.exports.index = async (req, res) => {
@@ -12,25 +12,38 @@ module.exports.index = async (req, res) => {
   const find = {
     deleted: false,
   };
-  
+
   if (req.query.status) {
     find.status = req.query.status;
   }
-  console.log(req.query.status);
 
   // Search function
   const objectSearch = searchrStatusHelper(req.query);
   if (objectSearch.regex) {
     find.title = objectSearch.regex;
   }
- 
 
-  const products = await Product.find(find);
+  // Pagination
+  const countProduct = await Product.countDocuments(find);
+  let objectPagination = paginationHelper(
+    {
+      currentPage: 1,
+      limitItem: 4,
+    },
+    req.query,
+    countProduct
+  );
+  // End Pagination
+
+  const products = await Product.find(find)
+    .limit(objectPagination.limitItem)
+    .skip(objectPagination.skip);
 
   res.render("admin/pages/products/index.pug", {
     pageTitle: "danh sách sản phẩm",
     products: products,
     filterStatus: filterStatus,
     keyword: objectSearch.keyword,
+    pagination: objectPagination,
   });
 };
--- product-management/public/admin/js/script.js
+++ product-management/public/admin/js/script.js
@@ -1,15 +1,15 @@
 // Button Status
 const buttonStatus = document.querySelectorAll("[button-status]");
 if (buttonStatus.length > 0) {
-  let url = new URL(window.location.href)
+  let url = new URL(window.location.href);
   buttonStatus.forEach((button) => {
     button.addEventListener("click", () => {
       const status = button.getAttribute("button-status");
-      
+
       if (status) {
-        url.searchParams.set("status", status)
+        url.searchParams.set("status", status);
       } else {
-        url.searchParams.delete("status")
+        url.searchParams.delete("status");
       }
       window.location.href = url.href;
     });
@@ -20,16 +20,31 @@ if (buttonStatus.length > 0) {
 // Form search
 const formSearch = document.querySelector("#form-search");
 if (formSearch) {
-  let url = new URL(window.location.href)
+  let url = new URL(window.location.href);
   formSearch.addEventListener("submit", (e) => {
     e.preventDefault();
     const keyword = e.target.elements.keyword.value;
     if (keyword) {
-      url.searchParams.set("keyword", keyword)
+      url.searchParams.set("keyword", keyword);
     } else {
-      url.searchParams.delete("keyword")
+      url.searchParams.delete("keyword");
     }
     window.location.href = url.href;
   });
 }
 // End form seach
+
+// Pagination
+const buttonsPagination = document.querySelectorAll("[button-pagination]");
+if (buttonsPagination) {
+  let url = new URL(window.location.href);
+
+  buttonsPagination.forEach((button) => {
+    button.addEventListener("click", () => {
+      const page = button.getAttribute("button-pagination");
+      url.searchParams.set("page", page);
+      window.location.href = url.href;
+    });
+  });
+}
+// End Pagination
