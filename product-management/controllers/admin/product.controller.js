01db6d2be5cf980cf9d37608862163a9480c15eb
quoc127 <nquoc160200@gmail.com>
2024-05-13 21:19:31 +0700

    25. delete product item in UI

--- product-management/controllers/admin/product.controller.js
+++ product-management/controllers/admin/product.controller.js
@@ -80,6 +80,10 @@ module.exports.changeMultiStatus = async (req, res) => {
 module.exports.deleteItem = async (req, res) => {
   const id = req.params.id;
 
-  await Product.deleteOne({ _id: id, });
+  // await Product.deleteOne({ _id: id, });
+  await Product.updateOne({ _id: id, }, {
+    deleted: true,
+    deletedAt: new Date()
+  });
   res.redirect("back");
 };
