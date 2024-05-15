12c4a0504e5240445c9bdac9976c75adb83d4441
quoc127 <nquoc160200@gmail.com>
2024-05-15 09:07:33 +0700

    27.admin - preview image when upload

--- product-management/public/admin/js/script.js
+++ product-management/public/admin/js/script.js
@@ -141,3 +141,22 @@ if (showAlert) {
 }
 // End Show Alert
 
+// Upload Image
+const uploadImage = document.querySelector("[upload-image]");
+if (uploadImage) {
+  const uploadImageInput = document.querySelector("[upload-image-input]");
+  const uploadImagePreview = document.querySelector("[upload-image-preview]");
+
+  uploadImageInput.addEventListener("change", (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      uploadImagePreview.src = URL.createObjectURL(file);
+    }
+  });
+}
+// End Upload Image
+
+// Remove Image
+
+// End Remove Image
+
