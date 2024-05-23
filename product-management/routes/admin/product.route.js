8516eb468d0cc9e28c95c1a6e1263ebec43867f1
quoc127 <nquoc160200@gmail.com>
2024-05-23 09:46:05 +0700

    30.admin - push image file from Cloudinary

--- product-management/routes/admin/product.route.js
+++ product-management/routes/admin/product.route.js
@@ -1,13 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const multer = require("multer");
-const storageMulter = require("../../helpers/storageMulter");
-const upload = multer({ storage: storageMulter() });
-
+const upload = multer();
 const controller = require("../../controllers/admin/product.controller");
-
 const validate = require("../../validates/admin/product.validate");
-
+const uplodaCloud = require("../../middlewares/admin/uploadCould.middleware")
 router.get("/", controller.index);
 
 router.patch("/change-status/:status/:id", controller.changeStatus);
@@ -16,7 +13,12 @@ router.patch("/change-multi", controller.changeMultiStatus);
 router.delete("/delete/:id", controller.deleteItem);
 
 router.get("/create", controller.create);
-router.post("/create", upload.single("thumbnail"), validate.createPost, controller.createPost);
+router.post(
+  "/create",
+  upload.single("thumbnail"),
+  uplodaCloud.upload,
+  validate.createPost,
+  controller.createPost);
 
 router.get("/edit/:id", controller.edit);
 router.patch("/edit/:id", upload.single("thumbnail"), validate.createPost, controller.editPatch);
