86100cef52c8ba94da8fe0f58849c4ad053c6b83
quoc127 <nquoc160200@gmail.com>
2024-06-06 09:59:26 +0700

    36.admin - update create account page

--- product-management/controllers/admin/account.controller.js
+++ product-management/controllers/admin/account.controller.js
@@ -1,12 +1,22 @@
 const Account = require("../../models/account.model");
+const Role = require("../../models/role.model");
 const systemConfig = require("../../config/system");
-const md5 = require('md5');
+const md5 = require("md5");
+
 // [GET] /admin/accounts
 module.exports.index = async (req, res) => {
   let find = {
     deleted: false,
   };
-  const records = await Account.find(find);
+  const records = await Account.find(find).select("-password -token");
+
+  for (const record of records) {
+    const role = await Role.findOne({ 
+      _id: record.role_id,
+      deleted: false 
+    });
+    record.role = role;
+  }
 
   res.render("admin/pages/accounts/index.pug", {
     pageTitle: "Danh sách tài khoản",
@@ -16,17 +26,29 @@ module.exports.index = async (req, res) => {
 
 // [GET] /admin/accounts/create
 module.exports.create = async (req, res) => {
+  const roles = await Role.find({ deleted: false });
   res.render("admin/pages/accounts/create.pug", {
     pageTitle: "Tạo mới tài khoản",
+    roles: roles,
   });
 };
 
 // [POST] /admin/accounts/create
 module.exports.createPost = async (req, res) => {
-  req.body.password = md5(req.body.password);
+  const emailExist = await Account.findOne({
+    email: req.body.email,
+    deleted: false,
+  });
 
-  const record = new Account(req.body)
-  await record.save();
+  if (emailExist) {
+    req.flash("error", `Email ${req.body.email} đã tồn tại!`);
+    res.redirect("back");
+  } else {
+    req.body.password = md5(req.body.password);
 
-  res.redirect(`${systemConfig.prefixAdmin}/accounts`)
+    const record = new Account(req.body);
+    await record.save();
+
+    res.redirect(`${systemConfig.prefixAdmin}/accounts`);
+  }
 };
