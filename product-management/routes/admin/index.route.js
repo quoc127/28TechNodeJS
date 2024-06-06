49c32027223e1eea903c80a493e50e9215c0047b
quoc127 <nquoc160200@gmail.com>
2024-06-06 22:16:57 +0700

    36.admin - update UI and add middleware for route

--- product-management/routes/admin/index.route.js
+++ product-management/routes/admin/index.route.js
@@ -1,5 +1,7 @@
 const systemConfig = require("../../config/system");
 
+const authMiddleware = require("../../middlewares/admin/auth.middleware");
+
 const dashboardRoutes = require("./dashboard.route");
 const productdRoutes = require("./product.route");
 const productCategorydRoutes = require("./product-category.route");
@@ -7,20 +9,26 @@ const roleRoutes = require("./role.route");
 const accountRoutes = require("./account.route");
 const authRoutes = require("./auth.route");
 
-
 module.exports = (app) => {
   const PATH_ADMIN = systemConfig.prefixAdmin;
 
-  app.use(PATH_ADMIN + "/dashboard", dashboardRoutes);
-  
-  app.use(PATH_ADMIN + "/products", productdRoutes);
+  app.use(
+    PATH_ADMIN + "/dashboard",
+    authMiddleware.requireAuth,
+    dashboardRoutes
+  );
 
-  app.use(PATH_ADMIN + "/products-category", productCategorydRoutes);
+  app.use(PATH_ADMIN + "/products", authMiddleware.requireAuth, productdRoutes);
 
-  app.use(PATH_ADMIN + "/roles", roleRoutes);
+  app.use(
+    PATH_ADMIN + "/products-category",
+    authMiddleware.requireAuth,
+    productCategorydRoutes
+  );
 
-  app.use(PATH_ADMIN + "/accounts", accountRoutes);
+  app.use(PATH_ADMIN + "/roles", authMiddleware.requireAuth, roleRoutes);
+
+  app.use(PATH_ADMIN + "/accounts", authMiddleware.requireAuth, accountRoutes);
 
   app.use(PATH_ADMIN + "/auth", authRoutes);
-
 };
