a97af224b3ccc4ae25d57878e9a8cf491e7f1d33
quoc127 <nquoc160200@gmail.com>
2024-06-10 23:35:03 +0700

    39.client - update UI detail products page

--- product-management/controllers/client/product.controller.js
+++ product-management/controllers/client/product.controller.js
@@ -18,16 +18,27 @@ module.exports.index = async (req, res) => {
   });
 };
 
-// [GET] /products/:slug
+// [GET] /products/detail/:slug
 module.exports.detail = async (req, res) => {
   try {
     const find = {
       deleted: false,
-      slug: req.params.slug,
+      slug: req.params.slugProduct,
       status: "active",
     };
 
     const product = await Product.findOne(find);
+
+    if(product.product_category_id) {
+      const category = await ProductCategory.findOne({
+        _id: product.product_category_id,
+        status: "active",
+        deleted: false
+      })
+      product.category = category
+    }
+
+    product.priceNew = productsHelper.priceNewProduct(product)
 
     res.render("client/pages/products/detail.pug", {
       pageTitle: product.title,
--- product-management/helpers/products.js
+++ product-management/helpers/products.js
@@ -1,9 +1,12 @@
 module.exports.priceNewProducts = (products) => {
   products.map((item) => {
     item.priceNew = (
-      (item.price * (100 - item.discountPercentage)) /
-      100
-    ).toFixed(0);
+      (item.price * (100 - item.discountPercentage)) / 100).toFixed(0);
   });
-  return products;
+};
+
+module.exports.priceNewProduct = (product) => {
+ const priceNew = ((product.price * (100 - product.discountPercentage)) / 100).toFixed(0);
+
+ return priceNew;
 };
