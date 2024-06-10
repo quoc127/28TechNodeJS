3daeb01b6851bc8b18f8c534a6e503eae82c66f5
quoc127 <nquoc160200@gmail.com>
2024-06-10 10:00:42 +0700

    39.client - show the latest products list

--- product-management/controllers/client/home.controller.js
+++ product-management/controllers/client/home.controller.js
@@ -3,6 +3,7 @@ const productsHelper = require("../../helpers/products");
 
 // [GET] /
 module.exports.index = async (req, res) => {
+  // Get list products featured
   const productsFeatured = await Products.find({
     deleted: false,
     featured: "1",
@@ -10,9 +11,18 @@ module.exports.index = async (req, res) => {
   });
 
   productsHelper.priceNewProducts(productsFeatured);
+
+  // Get the latest products list
+  const productsNew = await Products.find({
+    deleted: false,
+    status: "active",
+  }).sort({ position: "desc"}).limit(6);
+
+  productsHelper.priceNewProducts(productsNew);
   
   res.render("client/pages/home/index.pug", {
     pageTitle: "trang chủ",
     productsFeatured: productsFeatured,
+    productsNew: productsNew
   });
 };
