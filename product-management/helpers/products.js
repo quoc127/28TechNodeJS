4b609c30f29529ccef4c096c41e8be48c2da060b
quoc127 <nquoc160200@gmail.com>
2024-06-11 20:52:16 +0700

    40.client - search function

--- product-management/helpers/products.js
+++ product-management/helpers/products.js
@@ -1,12 +1,14 @@
 module.exports.priceNewProducts = (products) => {
   products.map((item) => {
     item.priceNew = (
-      (item.price * (100 - item.discountPercentage)) / 100).toFixed(0);
+      (item.price * (100 - item.discountPercentage)) / 100 ).toFixed(0);
+      return item;
   });
 };
 
 module.exports.priceNewProduct = (product) => {
- const priceNew = ((product.price * (100 - product.discountPercentage)) / 100).toFixed(0);
+  const priceNew = (
+    (product.price * (100 - product.discountPercentage)) / 100 ).toFixed (0);
 
- return priceNew;
+  return priceNew;
 };
