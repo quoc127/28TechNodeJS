db370bef01bcbf69f8e874630edb754aa05a6749
quoc127 <nquoc160200@gmail.com>
2024-06-11 22:34:41 +0700

    40.client - update search function

--- product-management/public/js/script.js
+++ product-management/public/js/script.js
@@ -13,3 +13,20 @@ if (showAlert) {
   });
 }
 // End Show Alert
+
+// Form search
+const formSearch = document.querySelector("#form-search");
+if (formSearch) {
+  let url = new URL(window.location.href);
+  formSearch.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const keyword = e.target.elements.keyword.value;
+    if (keyword) {
+      url.searchParams.set("keyword", keyword);
+    } else {
+      url.searchParams.delete("keyword");
+    }
+    window.location.href = url.href;
+  });
+}
+// End form seach
