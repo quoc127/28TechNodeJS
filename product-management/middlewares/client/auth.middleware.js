36b593278d51f04f119ff4d103e33f4539d6f83c
quoc127 <nquoc160200@gmail.com>
2024-06-24 20:15:36 +0700

    43.client - show info user on header and redirect login page if not  login

---
+++ product-management/middlewares/client/auth.middleware.js
@@ -0,0 +1,14 @@
+const User = require("../../models/user.model");
+
+module.exports.requireAuth = async (req, res, next) => {
+  if (!req.cookies.token) {
+    res.redirect(`/user/login`);
+    return;
+  }
+  const user = await User.findOne({ tokenUser: req.cookies.tokenUser });
+  if (!user) {
+    res.redirect(`/user/login`);
+    return;
+  }
+  next();
+};
--- product-management/routes/client/user.route.js
+++ product-management/routes/client/user.route.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const controller = require("../../controllers/client/user.controller.js");
 const validate = require("../../validates/client/user.validate.js");
+const authMiddleware = require("../../middlewares/client/auth.middleware.js")
 
 router.get("/register", controller.register);
 router.post("/register", validate.registerPost, controller.registerPost);
@@ -24,5 +25,7 @@ router.post("/password/otp", controller.otpPasswordPost);
 
 router.get("/password/reset", controller.resetPassword);
 router.post("/password/reset", validate.resetPasswordPost, controller.resetPasswordPost);
+
+router.get("/info", authMiddleware.requireAuth, controller.info);
 
 module.exports = router;
