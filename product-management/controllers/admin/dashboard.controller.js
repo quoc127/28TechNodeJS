527db8e001d3cf229347c08b7e871d8ab2b99e70
quoc127 <nquoc160200@gmail.com>
2024-06-24 22:54:03 +0700

    43.admin - update UI overview page

--- product-management/controllers/admin/dashboard.controller.js
+++ product-management/controllers/admin/dashboard.controller.js
@@ -1,6 +1,33 @@
+const ProductCategory = require("../../models/product-category.model")
 // [GET] /admin/dashboard
-module.exports.dashboard = (req, res) => {
+module.exports.dashboard = async (req, res) => {
+  const statistic = {
+    categoryProducts: {
+      total: 0,
+      active: 0,
+      inactive: 0,
+    },
+    products: {
+      total: 0,
+      active: 0,
+      inactive: 0,
+    },
+    account: {
+      total: 0,
+      active: 0,
+      inactive: 0,
+    },
+    user: {
+      total: 0,
+      active: 0,
+      inactive: 0,
+    },
+  }
+  statistic.categoryProducts.total = await ProductCategory.countDocuments({ deleted: false })
+  statistic.categoryProducts.active = await ProductCategory.countDocuments({ status: "active",deleted: false })
+  statistic.categoryProducts.inactive = await ProductCategory.countDocuments({ status: "inactive",deleted: false })
   res.render("admin/pages/dashboard/index.pug", {
-    pageTitle: "trang tổng quan"
+    pageTitle: "Trang tổng quan",
+    statistic: statistic
   });
 }
