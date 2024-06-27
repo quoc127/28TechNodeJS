445756d341bebb5c17883e24e751ebc62a219b1e
quoc127 <nquoc160200@gmail.com>
2024-06-27 21:54:40 +0700

    44.client - create UI box chat

--- product-management/routes/client/index.route.js
+++ product-management/routes/client/index.route.js
@@ -9,6 +9,7 @@ const searchRoutes = require("./search.route");
 const cartRoutes = require("./cart.route");
 const checkoutRoutes = require("./checkout.route");
 const userRoutes = require("./user.route");
+const chatRoutes = require("./chat.route");
 
 module.exports = (app) => {
   app.use(categoryMiddleware.category)
@@ -27,4 +28,6 @@ module.exports = (app) => {
   app.use("/checkout", checkoutRoutes);
 
   app.use("/user", userRoutes);
+
+  app.use("/chat", chatRoutes);
 };
