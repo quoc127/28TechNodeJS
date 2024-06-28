ce32fa1f809d0a52769d9bf9488b4d11de34b6c4
quoc127 <nquoc160200@gmail.com>
2024-06-29 00:11:18 +0700

    44.client - update chat box

--- product-management/public/js/chat.js
+++ product-management/public/js/chat.js
@@ -1,1 +1,34 @@
-var socket = io();
+// CLIENT_SEND_MESSAGE
+const formSendData = document.querySelector(".chat .inner-form");
+if (formSendData) {
+  formSendData.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const content = e.target.elements.content.value;
+    if (content) {
+      socket.emit("CLIENT_SEND_MESSAGE", content);
+      e.target.elements.content.value = "";
+    }
+  });
+}
+// END_CLIENT_SEND_MESSAGE
+
+// SERVER RETURN MESSAGE
+socket.on("SERVER_RETURN_MESSAGE", (data) => {
+  const myId = document.querySelector("[my-id]").getAttribute("my-id")
+  const body = document.querySelector(".chat .inner-body");
+  const div = document.createElement("div");
+  let htmlFullName = "";
+
+  if (myId == data.userId) {
+    div.classList.add("inner-outgoing");
+  } else {
+    div.classList.add("inner-incoming");
+    htmlFullName = `<div class="inner-name">${data.fullName}</div>`
+  }
+  div.innerHTML = `
+    ${htmlFullName}
+    <div class="inner-content">${data.content}</div>
+  `;
+  body.appendChild(div);
+});
+// END SERVER RETURN MESSAGE
