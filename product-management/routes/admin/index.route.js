7f75903663c8810a6d3538a717089aca99aad0ba
quoc127 <nquoc160200@gmail.com>
2024-06-29 20:08:01 +0700

    45.admin - update route admin login

--- product-management/routes/admin/index.route.js
+++ product-management/routes/admin/index.route.js
@@ -10,9 +10,12 @@ const accountRoutes = require("./account.route");
 const authRoutes = require("./auth.route");
 const myAccountRoutes = require("./my-account.route");
 const settingRoutes = require("./setting.route");
+const authController = require("../../controllers/admin/auth.controller");
 
 module.exports = (app) => {
   const PATH_ADMIN = systemConfig.prefixAdmin;
+
+  app.get(PATH_ADMIN, authController.login)
 
   app.use(
     PATH_ADMIN + "/dashboard",
