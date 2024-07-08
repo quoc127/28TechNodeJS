045bc7000b7b372888bcbcaa1f271ddba939c790
quoc127 <nquoc160200@gmail.com>
2024-07-08 22:30:27 +0700

    46.client - upload multi image

--- product-management/controllers/client/chat.controller.js
+++ product-management/controllers/client/chat.controller.js
@@ -1,6 +1,7 @@
 const Chat = require("../../models/chat.model");
 const User = require("../../models/user.model");
 
+const uploadToCloudinary = require("../../helpers/uploadToCloudinary")
 // [GET] /chat
 module.exports.index = async (req, res) => {
   const userId = res.locals.user.id;
@@ -8,10 +9,17 @@ module.exports.index = async (req, res) => {
 
   // SocketIO
   _io.once("connection", (socket) => {
-    socket.on("CLIENT_SEND_MESSAGE", async (content) => {
+    socket.on("CLIENT_SEND_MESSAGE", async (data) => {
+      let images = [];
+      for(const imageBuffer of data.images) {
+        const link = await uploadToCloudinary(imageBuffer);
+        images.push(link)
+      }
+
       const chat = new Chat({
         user_id: userId,
-        content: content,
+        content: data.content,
+        images: images
       });
       await chat.save();
 
@@ -19,7 +27,8 @@ module.exports.index = async (req, res) => {
       _io.emit("SERVER_RETURN_MESSAGE", {
         userId: userId,
         fullName: fullName,
-        content: content,
+        content: data.content,
+        images: images
       });
     });
 
--- product-management/public/js/chat.js
+++ product-management/public/js/chat.js
@@ -1,15 +1,29 @@
 import * as Popper from "https://cdn.jsdelivr.net/npm/@popperjs/core@^2/dist/esm/index.js";
 var timeOut;
 
+// File Uploda With Preview
+const upload = new FileUploadWithPreview.FileUploadWithPreview('upload-image', {
+  multiple: true,
+  maxFileCount: 5
+});
+// End File Uploda With Preview
+
 // CLIENT_SEND_MESSAGE
 const formSendData = document.querySelector(".chat .inner-form");
 if (formSendData) {
   formSendData.addEventListener("submit", (e) => {
     e.preventDefault();
     const content = e.target.elements.content.value;
-    if (content) {
-      socket.emit("CLIENT_SEND_MESSAGE", content);
+    const images = upload.cachedFileArray || [];
+    console.log(images);
+
+    if (content || images.length > 0) {
+      socket.emit("CLIENT_SEND_MESSAGE", {
+        content: content,
+        images: images
+      });
       e.target.elements.content.value = "";
+      upload.resetPreviewPanel();
       socket.emit("CLIENT_SEND_TYPING", "hidden");
     }
   });
@@ -23,6 +37,8 @@ socket.on("SERVER_RETURN_MESSAGE", (data) => {
   const boxTyping = document.querySelector(".inner-list-typing");
   const div = document.createElement("div");
   let htmlFullName = "";
+  let htmlContent = "";
+  let htmlImages = "";
 
   if (myId == data.userId) {
     div.classList.add("inner-outgoing");
@@ -30,10 +46,32 @@ socket.on("SERVER_RETURN_MESSAGE", (data) => {
     div.classList.add("inner-incoming");
     htmlFullName = `<div class="inner-name">${data.fullName}</div>`;
   }
+
+  if (data.content) {
+    htmlContent = `
+      <div class="inner-content">${data.content}</div>
+    `
+  }
+
+  if (data.images) {
+    htmlImages += `<div class="inner-images">`
+    
+    for(const image of data.images) {
+      htmlImages += `
+        <img src="${image}"
+      `
+    
+    htmlImages += `</div>`
+    }
+  }
+
   div.innerHTML = `
     ${htmlFullName}
-    <div class="inner-content">${data.content}</div>
+    ${htmlContent}
+    ${htmlImages}
   `;
+
+  // window.location.reload();
   body.insertBefore(div, boxTyping);
   body.scrollTop = body.scrollHeight;
 });
